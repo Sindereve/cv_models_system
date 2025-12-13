e51768865a78e2b4f6c7359ae475b79b89ef7a36
sindik_argo <sindereve850live@gmail.com>
2025-12-13 12:59:55 +0500

    feat: create class load EfficientNet

---
+++ trainer/src/task/classification/models/efficientnet.py
@@ -0,0 +1,87 @@
+from torch import nn
+from .registry import register
+from torchvision.models import (
+    efficientnet_b0, efficientnet_b1, efficientnet_b2,
+    efficientnet_b3, efficientnet_b4, efficientnet_b5,
+    efficientnet_b6, efficientnet_b7, efficientnet_v2_s,
+    efficientnet_v2_m, efficientnet_v2_l,
+    EfficientNet as TorchvisionEfficientNet
+)
+
+model_mapping = {
+    "efficientnet_b0": efficientnet_b0,
+    "efficientnet_b1": efficientnet_b1,
+    "efficientnet_b2": efficientnet_b2,
+    "efficientnet_b3": efficientnet_b3,
+    "efficientnet_b4": efficientnet_b4,
+    "efficientnet_b5": efficientnet_b5,
+    "efficientnet_b6": efficientnet_b6,
+    "efficientnet_b7": efficientnet_b7,
+    "efficientnet_v2_s": efficientnet_v2_s,
+    "efficientnet_v2_m": efficientnet_v2_m,
+    "efficientnet_v2_l": efficientnet_v2_l
+}
+
+@register("efficientnet")
+class EfficientNet(nn.Module):
+    def __init__(
+            self, 
+            num_class: int,
+            name: str = 'efficientnet_b0', 
+            weights: bool = False,
+        ):
+        """
+        Model EfficientNet
+        
+        :param num_class: Number of output classes.
+        :type num_class: int
+        :param name: Name of the EfficientNet model to load.
+        :type name: str
+        :param weights: Whether to load pretrained weights.
+        :type weights: bool
+        """
+        super().__init__()
+
+        self.model = self._load_model(name, weights)
+        self.model_name = name
+
+        in_features = self.model.classifier[1].in_features
+        self.model.classifier[1] = nn.Linear(in_features, num_class)
+
+    def forward(self, x):
+        return self.model(x)
+    
+    def get_name_model(self):
+        """
+        Get model name
+        """
+        return self.model_name
+    
+    def _load_model(
+            self,
+            model_name: str,
+            weights: bool
+        ) -> TorchvisionEfficientNet:
+        """
+        Load model EfficientNet white torchvision
+        
+        :param model_name: Name of the EfficientNet model to load.
+        :type model_name: str
+        :param weights: Whether to load pretrained weights.
+        :type weights: bool
+        
+        :return: Instantiated EfficientNet model.
+        :rtype: TorchvisionEfficientNet
+        """
+        
+        if model_name not in model_mapping:
+            raise ValueError(f"Unknown model name: {model_name}. Available: {list(model_mapping.keys())}")
+
+        model: TorchvisionEfficientNet = model_mapping[model_name](weights="DEFAULT" if weights else None)
+        
+        # Заморозка весов
+        if weights:
+            for param in model.parameters():
+                param.requires_grad = False
+
+        return model
